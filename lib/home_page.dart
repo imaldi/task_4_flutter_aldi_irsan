4c5d6d7bfe636558cbc6092abc1dbd9d13bebe15
im_aldi <aldiirsanmajid@gmail.com>
2020-12-01 11:18:42 +0700

    Initial commit

---
+++ lib/home_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'main.dart';
+
+class HomePage extends StatelessWidget{
+  final String username;
+  final String password;
+  // wajib diisi
+  HomePage({Key key,this.username,this.password}) : super(key: key);
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text('Covid 19 News'),
+        ),
+      ),
+      body: Center(
+        child: ListView(
+            padding: EdgeInsets.only(top: 50.0),
+            children: <Widget>[
+              Container(
+              child: Center(
+                  child:
+                    Text("Hello "+username,
+                      style: TextStyle(
+                        fontSize: 18.0,
+                      ),),
+                ),
+              ),
+              SizedBox(height: 10,),
+              Container(
+                child: Center(
+                  child:
+                  Text("Password anda : "+password,
+                    style: TextStyle(
+                      fontSize: 18.0,
+                    ),)
+                ),
+              ),
+            ]
+        ),
+      ),
+    ); // This trailin
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Login Beginner',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 5, 30, 30),
+        child: Center(
+          child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: usernameController,
+                validator: (String value) {
+                  if (value.isEmpty) {
+                    return "Email Harus Diisi";
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                  hintText: "Email",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(4.0)),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                controller: passwordController,
+                validator: (String value) {
+                  if (value.isEmpty) {
+                    return "Password tidak boleh kosong";
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                  hintText: "Password",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(4.0)),
+                ),
+                obscureText: true,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              RaisedButton(
+                child: Text("LOGIN"),
+                onPressed: () {
+                  if(_formKey.currentState.validate()){
+                    if(usernameController.text == "admin" && passwordController.text == "admin"){
+                      Navigator.pushReplacement(context,
+                          MaterialPageRoute(builder: (context) => HomePage(username : usernameController.text, password : passwordController.text))
+                      );
+                    } else {
+                      Fluttertoast.showToast(
+                          msg: "Password atau Username Salah",
+                          toastLength: Toast.LENGTH_SHORT,
+                          gravity: ToastGravity.CENTER,
+                          timeInSecForIos: 1,
+                          backgroundColor: Colors.green,
+                          textColor: Colors.white,
+                          fontSize: 16.0
+                      );
+                    }
+                  }
+                },
+              ),
+              SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
+        ),
+      ),), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
